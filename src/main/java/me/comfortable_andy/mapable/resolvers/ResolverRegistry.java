14264991c7e3771fa4af6fd881d18fba0cf2a618
AndyNoob <69229995+AndyNoob@users.noreply.github.com>
2023-02-13 21:39:10 -0800

    Changed the way resolver finding works

--- src/main/java/me/comfortable_andy/mapable/resolvers/ResolverRegistry.java
+++ src/main/java/me/comfortable_andy/mapable/resolvers/ResolverRegistry.java
@@ -7,8 +7,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unchecked")
 public final class ResolverRegistry {
@@ -35,20 +37,19 @@ public final class ResolverRegistry {
     }
 
     @Nullable
-    public <R> R ifPresent(final @NotNull Class<?> clazz, @NotNull final Function<IResolver, R> function) {
-        final IResolver resolver = this.resolvers.stream().filter(iResolver -> iResolver.getResolvableTypes().stream().anyMatch(clazs -> clazs.isAssignableFrom(clazz))).findFirst().orElse(null);
-        if (resolver == null) return null;
+    public <R> R ifPresent(final @NotNull Class<?> clazz, @NotNull final Function<List<IResolver>, R> function) {
+        final List<IResolver> resolver = this.resolvers.stream().filter(iResolver -> iResolver.getResolvableTypes().stream().anyMatch(clazs -> clazs.isAssignableFrom(clazz))).collect(Collectors.toList());
         return function.apply(resolver);
     }
 
     @Nullable
     public ResolvedField resolve(final @NotNull Class<?> clazz, final @NotNull ResolvableField field) {
-        return ifPresent(clazz, resolver -> resolver.resolve(field));
+        return ifPresent(clazz, resolvers -> resolvers.stream().map(resolver -> resolver.resolve(field)).filter(Objects::nonNull).findFirst().orElse(null));
     }
 
     @Nullable
     public <T> T unresolve(final @NotNull Class<T> clazz, final @NotNull ResolvedField field, final @NotNull FieldInfo info) {
-        return (T) ifPresent(clazz, resolver -> resolver.unresolve(field, info));
+        return (T) ifPresent(clazz, resolvers -> resolvers.stream().map(resolver -> resolver.unresolve(field, info)).filter(Objects::nonNull).findFirst().orElse(null));
     }
 
     public static ResolverRegistry getInstance() {
